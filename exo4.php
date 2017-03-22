c653d9734f9a3f85ed1d3ab021d4d9aa216ff20b
Gena <gena@e64.net>
2017-03-22 21:45:29 +0100

    Correction exercice 4 : gestion des methodes get et post dans la page user

---
+++ exo4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo2</title>
+</head>
+<body>
+	<form action="user.php" method= "post">
+		<label for ='name'>Nom</label>
+		<input type="text" name = "name">
+		<label for ="firstName"> Prenom</label>
+		<input type="text" name="firstName">
+		<input type = "submit" value = "envoyer">
+	</form>
+	
+
+</body>
+</html>
--- user.php
+++ user.php
@@ -1,7 +1,19 @@
 <?php
-	//echo $_POST["name"];
-	//echo $_POST["firstName"];
 
-echo $_GET["name"];
-echo $_GET["firstName"];
+echo 'Informations récupérées dans le $_REQUEST quelque soit la méthode : ';
+echo $_REQUEST["name"];
+echo $_REQUEST["firstName"];
+
+echo "<br/><br/> Méthode utilisée dans le formulaire pour appeler la page : ".$_SERVER['REQUEST_METHOD']."<br/>";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	echo 'Informations récupérées dans le $_POST : ';
+	echo $_POST["name"];
+	echo $_POST["firstName"];
+}
+else{
+	echo 'Informations récupérées dans le $_GET : ';
+	echo $_GET["name"];
+	echo $_GET["firstName"];
+}
 ?>
