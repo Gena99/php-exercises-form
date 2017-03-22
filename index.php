95764194508f3a60700a0834893317b05c26e1fb
Gena <gena@e64.net>
2017-03-22 22:23:51 +0100

    exo6

--- index.php
+++ index.php
@@ -5,22 +5,29 @@
 	<title>exo5</title>
 </head>
 <body>
-	<form action="index.php" method= "post">
-		<select>
+	
+	<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		echo 'Informations récupérées dans le $_POST : <br/>';
+		echo $_POST["civilite"] . " " . $_POST["name"] . " " . $_POST["firstName"];
+	}else{
+		echo ' <form action="index.php" method= "post">
+		<select name="civilite">
 			<option value = "Monsieur">Monsieur</option>
 			<option value = "Madame">Madame</option>
 		</select>
-		<label for ='name'>Nom</label>
+		<label for ="name">Nom</label>
 		<input type="text" name = "name">
 		<label for ="firstName"> Prenom</label>
 		<input type="text" name="firstName">
 		<input type = "submit" value = "envoyer">
-	</form>
-	<?php
-	echo $_POST["civilite"];
-	echo $_POST["name"];
-	echo $_POST["firstName"];
-	?>
+	</form>';
+}
+
+
+?>
+
+
 
 </body>
 </html>
